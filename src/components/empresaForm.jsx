551a4d1efbacf3a0165a8462739e03d46b86b17b
Alejandro <alebernal06065@gmail.com>
2025-10-03 10:05:52 -0300

    Se agregaron nuevos atributos tanto en la base de datos como en el formulario

--- src/components/empresaForm.jsx
+++ src/components/empresaForm.jsx
@@ -6,12 +6,15 @@ import { useState } from 'react'
 const EmpresaForm = ({ onAdd }) => {
   const [nombre, setNombre] = useState('')
   const [descripcion, setDescripcion] = useState('')
-
+  const [website, setWebsite] = useState('')
+  const [email, setEmail] = useState('')
   const handleSubmit = (e) => {
     e.preventDefault()
-    onAdd({ nombre, descripcion })
+    onAdd({ nombre, descripcion, website, email })
     setNombre('')
     setDescripcion('')
+    setWebsite('')
+    setEmail('')
   }
 
   return (
@@ -23,6 +26,14 @@ const EmpresaForm = ({ onAdd }) => {
       <div>
         <label>Descripción:</label>
         <input value={descripcion} onChange={e => setDescripcion(e.target.value)} />
+      </div>
+      <div>
+        <label>Website:</label>
+        <input value={website} onChange={e => setWebsite(e.target.value)} />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input value={email} onChange={e => setEmail(e.target.value)} type="email" />
       </div>
       <button type="submit">Agregar Empresa</button>
     </form>
