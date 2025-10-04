ce4e8dc34d6a6146db3ad709049b1d37a2f4bb60
Alejandro <alebernal06065@gmail.com>
2025-10-03 21:22:50 -0300

    Se cambio la toma de atributos, como el backend. De igual forma hay errores. Pero ya los voy a encontrar en el proximo commit

--- src/components/avisoForm.jsx
+++ src/components/avisoForm.jsx
@@ -7,18 +7,26 @@ const AvisoForm = ({ empresas, onAdd }) => {
   const [titulo, setTitulo] = useState('')
   const [descripcion, setDescripcion] = useState('')
   const [ubicacion, setUbicacion] = useState('')
-  const [tipodecontrato, setTipodecontrato] = useState('')
+  const [tipo_contrato, setTipoContrato] = useState('')
   const [empresa_id, setEmpresaId] = useState('')
   const [fecha, setFecha] = useState('')
   const [empresa_nombre, setEmpresaNombre] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    onAdd({ titulo, descripcion, ubicacion, tipodecontrato, empresa_id, empresa_nombre, fecha: fecha || new Date().toISOString().slice(0,10) })
+    onAdd({
+      titulo,
+      descripcion,
+      ubicacion,
+      tipo_contrato,
+      empresa_id: empresa_id ? Number(empresa_id) : '',
+      empresa_nombre,
+      fecha: fecha || new Date().toISOString().slice(0,10)
+    })
     setTitulo('')
     setDescripcion('')
     setUbicacion('')
-    setTipodecontrato('')
+    setTipoContrato('')
     setEmpresaId('')
     setFecha('')
   }
@@ -39,7 +47,7 @@ const AvisoForm = ({ empresas, onAdd }) => {
       </div>
       <div>
         <label>Tipo de Contrato:</label>
-        <select value={tipodecontrato} onChange={e => setTipodecontrato(e.target.value)} required>
+        <select value={tipo_contrato} onChange={e => setTipoContrato(e.target.value)} required>
           <option value="">Selecciona un tipo de contrato</option>
           <option value="full-time">Tiempo Completo</option>
           <option value="part-time">Tiempo Parcial</option>
@@ -48,7 +56,12 @@ const AvisoForm = ({ empresas, onAdd }) => {
       </div>
       <div>
         <label>Empresa:</label>
-        <input value={empresa_nombre} onChange={e => setEmpresaNombre(e.target.value)} required list="empresas-list" />
+        <select value={empresa_id} onChange={e => setEmpresaId(e.target.value)} required>
+          <option value="">Selecciona una empresa</option>
+          {empresas && empresas.map((empresa) => (
+            <option key={empresa.empresa_id} value={empresa.empresa_id}>{empresa.nombre}</option>
+          ))}
+        </select>
       </div>
       <div>
         <label>Fecha:</label>
